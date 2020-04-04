ec78a908b7d8bf5f97e9057ff072f18fbdb3d46b
patrickwoolf <h211133@nehs.hc.edu.tw>
2020-04-04 22:57:53 +0800

    removed tmp

--- scripts/curlresult.sh
+++
@@ -1,1 +0,0 @@
-"./m3u8-download https://zy.kubo-163-360.com/20200124/9781_4efc3913/index.m3u8 ../videos/ww1"
--- scripts/download-command.sh
+++
@@ -1,2 +0,0 @@
-bash  ./m3u8-download https://zy.kubo-163-360.com/20200124/9781_4efc3913/index.m3u8 ../videos/ww1
-du -h ../videos/ww* | cut -f1
