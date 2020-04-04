d5d7767b1af5da5c2fa13ca6b45568e171bd27ab
patrickwoolf <h211133@nehs.hc.edu.tw>
2020-04-04 12:49:57 +0000

    setup.sh remove getting into bash

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 # Install necessary packages, based on iOS 
 ## Please make sure you have everything installed.
 apk add curl ncurses bash youtube-dl ffmpeg grep
-bash
 cat /dev/location > /dev/null &.
