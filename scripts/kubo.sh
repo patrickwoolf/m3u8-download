228804cceb9de775c3dd47744a95c312fd354c65
patrickwoolf <h211133@nehs.hc.edu.tw>
2020-04-09 08:10:26 +0800

    kubo support

---
+++ scripts/kubo.sh
@@ -0,0 +1,8 @@
+rm -v curlresult.txt
+read -p "How many episodes are there in total? " ep
+for (( i=1; i<="$ep"; i++ )); do
+	while read -r line; do
+		echo "$line" >> curlresult.txt
+	done <<< $(curl `echo $1 | sed -e 's/pid\-./pid\-'$i'/g'` | grep -ohP https.*?index\.m3u8 | sed -e 's/https.*\%24//g' -e 's/\\//g')
+done
+echo "created curlresult.txt"
