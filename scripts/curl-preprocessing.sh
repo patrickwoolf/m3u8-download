113a4c9e15a2e7fb360dddcfd098b7c2d1ad34e4
patrickwoolf <h211133@nehs.hc.edu.tw>
2020-04-09 17:42:11 +0800

    Kubo multiple servers redundancy fixed

--- scripts/curl-preprocessing.sh
+++ scripts/curl-preprocessing.sh
@@ -4,4 +4,4 @@ rm -f download-command*
 mv curlresult.txt curlresulttmp.txt
 while read -r line; do
 	echo "$line" >> curlresult.txt
-done <<< $(cat curlresulttmp.txt | grep \.m3u8 | sed -e 's/'%3A'/:/g' -e 's/'%2F'/\//g')
+done <<< $(cat curlresulttmp.txt | grep \.m3u8 | sed -e 's/'%3A'/:/g' -e 's/'%2F'/\//g' -e '/\:.*\:/d')
