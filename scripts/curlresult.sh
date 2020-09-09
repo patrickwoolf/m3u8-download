623ace9e478087c02363ff39b75fe825c872ce5b
patrickwoolf <h211133@nehs.hc.edu.tw>
2020-09-10 01:21:46 +0800

    update gimy.sh

--- scripts/curlresult.sh
+++ scripts/curlresult.sh
@@ -1,12 +1,2 @@
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4680_ac7b42e2/index.m3u8 ../videos/billionss1e1"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4681_a1d3bf08/index.m3u8 ../videos/billionss1e2"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4682_4bc2ffca/index.m3u8 ../videos/billionss1e3"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4683_f65f6c52/index.m3u8 ../videos/billionss1e4"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4684_1f83f3b6/index.m3u8 ../videos/billionss1e5"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4685_66b083a8/index.m3u8 ../videos/billionss1e6"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4686_a4f938e8/index.m3u8 ../videos/billionss1e7"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4687_37686645/index.m3u8 ../videos/billionss1e8"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4688_f2b0ced0/index.m3u8 ../videos/billionss1e9"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4689_fb2c3c27/index.m3u8 ../videos/billionss1e10"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4690_eafb9c9b/index.m3u8 ../videos/billionss1e11"
-"./m3u8-download https://zy.kubozy-youku-163-aiqi.com/20190404/4691_300ef96d/index.m3u8 ../videos/billionss1e12"
+https://cdn-yong.bejingyongjiu.com/20200730/18182_f699d598/index.m3u8
+https://cdn-yong.bejingyongjiu.com/20200730/18183_3e9de0b6/index.m3u8
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # Install necessary packages, based on ubuntu/debian. 
 ## Please make sure you have everything installed.
-sudo apt install ffmpeg youtube-dl sed libncurses5-dbg
+sudo apt install ffmpeg youtube-dl sed libncurses5-dev
