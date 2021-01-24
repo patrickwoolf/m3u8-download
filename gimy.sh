08a02d1a15118547ea64f2ad178e90d9e1a82406
patrickwoolf <h211133@nehs.hc.edu.tw>
2021-01-24 21:05:31 +0800

    gimy.sh undo changes

--- gimy.sh
+++
@@ -1,9 +0,0 @@
-rm -v curlresult.txt
-read -p "Start episode from: " start
-read -p "End episode at: " ep
-for (( i="$start"; i<="$ep"; i++ )); do
-	while read -r line; do
-		echo "$line" >> curlresult.txt
-	done <<< $(curl `echo $1 | sed -e 's/.\.html/'$i'\.html/g'` | grep -ohP 'http[^ ]*index.m3u8' | grep -ohP http.*?index\.m3u8 | sed -e 's/\\//g' | sed -e '/\?/d') 
-done
-sed '$!N; /^\(.*\)\n\1$/!P; D' -i curlresult.txt
--- scripts/gimy.sh
+++ scripts/gimy.sh
@@ -4,6 +4,6 @@ read -p "End episode at: " ep
 for (( i="$start"; i<="$ep"; i++ )); do
 	while read -r line; do
 		echo "$line" >> curlresult.txt
-	done <<< $(curl `echo $1 | sed -e 's/.\.html/'$i'\.html/g'` | grep -ohP http.*?index\.m3u8 | sed -e 's/\\//g' | sed -e '/\?/d') 
+	done <<< $(curl `echo $1 | sed -e 's/.\.html/'$i'\.html/g'` | grep -ohP 'http[^ ]*index.m3u8' | grep -ohP http.*?index\.m3u8 | sed -e 's/\\//g' | sed -e '/\?/d') 
 done
 sed '$!N; /^\(.*\)\n\1$/!P; D' -i curlresult.txt
